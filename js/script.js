791948539314306233390ecf122c80116f6b12e4
Natalia Potracka <n.potracka@gmail.com>
2023-06-21 12:49:31 +0200

    add script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 
 function titleClickHandler(event){
   const clickedElement = this;
+  event.preventDefault();
   console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links  */
@@ -14,11 +15,9 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
-  
-
-  clickedElement.classList.add(select.all.class.active)
+  clickedElement.classList.add('active')
 
   console.log('clickedElement:', clickedElement);
 
@@ -30,11 +29,18 @@ for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
 
-  /* get 'href' attribute from the clicked link */
+  /* [IN PROGRESS] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
